cfc19ee90eba07304fe7fc44a217400b079f313d
ziyingdeng <dengziying98@gmail.com>
2025-08-24 22:01:32 -0400

    refine SeqSearchST

--- Chapter_3-1/_SequentialSearchST.java
+++ Chapter_3-1/_SequentialSearchST.java
@@ -2,19 +2,29 @@
 // search: O(N)
 // insert: (N)
 
+import edu.princeton.cs.algs4.Queue;
+
 public class _SequentialSearchST<Key, Value> {
     private Node first;
+    private int N;
 
     private class Node {
         Key key;
         Value val;
         Node next;
+
         public Node(Key k, Value v, Node n) {
             key = k;
             val = v;
             next = n;
         }
     }
+
+    public _SequentialSearchST() {
+            first = null;
+            N = 0;
+        }
+
 
     // Search the value corresponding to the given key in the list
     // return the found value or null
@@ -38,5 +48,39 @@ public class _SequentialSearchST<Key, Value> {
             } 
         }
         first = new Node(key, val, first);
+        N++;
     }
+
+    public int size() { return N; }
+
+        public void delete(Key key) {
+            // empty
+            if (first == null) return;
+            // only one node
+            if (key.equals(first.key)) {
+                first = first.next;
+                N--;
+                return;
+            }
+
+            Node prev = first;
+            Node curr = first.next;
+            while (curr != null) {
+                if (key.equals(curr.key)) {
+                    prev.next = curr.next;
+                    N--;
+                    return;
+                }
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        public Iterable<Key> keys() {
+            Queue<Key> q = new Queue<>();
+            for (Node x = first; x != null; x = x.next) {
+                q.enqueue(x.key);
+            }
+            return q;
+        }
 }
